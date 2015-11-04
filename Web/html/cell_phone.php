c5dd9ebcf7f426c87c52b9d3caebb57b204cdc39
root <root@blood>
2015-11-04 14:29:42 -0700

    Converted all html to be php based

---
+++ Web/html/cell_phone.php
@@ -0,0 +1,17 @@
+<?php include 'included/header.php'; ?>
+<?php include 'included/navbar.php'; ?>
+
+<section>
+	<ul>
+		<li><a href="">Apple</a></li>
+		<li><a href="">Blackberry</a></li>
+                <li><a href="">HTC</a></li>
+                <li><a href="">LG</a></li>
+                <li><a href="">Motorola</a></li>
+                <li><a href="">Nokia</a></li>
+                <li><a href="">Samsung</a></li>
+	</ul>
+</section>
+
+
+<?php include 'included/footer.php'; ?>
---
+++ Web/html/included/navbar.php
@@ -0,0 +1,10 @@
+<nav>
+	<ul>
+		<li><a href="iPhone.php">iPhone</a></li>
+		<li><a href="cell_phone.php">Cell Phone</a></li>
+		<li><a href="Tablet.php">Tablet</a></li>
+		<li><a href="iPod.php">iPod</a></li>
+		<li><a href="Computer.php">Computer</a></li>
+	</ul>
+</nav>
+
