1d69acf163576ec27fcddcff87e04df9cba360ed
Michael Swisher <Michael.V.Swisher@colorado.edu>
2015-11-11 14:45:38 -0700

    Updating the scraper to include a sample reference scraper in comments. Also breaking the program apart to seperate the different website scrapers into sections.

--- Scripts/scraper.py
+++ Scripts/scraper.py
@@ -1,26 +1,50 @@
 from bs4 import BeautifulSoup
 import requests
+import urllib
+###################################################################
+Reference Scraper
+###################################################################
+'''
+url = ""
+r = requests.get(url)
+data = r.text
+soup = BeautifulSoup(data, "html.parser")
 
-def url_scraper(url):
-        r = requests.get(url)
-        data = r.text
-        soup = BeautifulSoup(data, "html.parser")
-        links = []
-        for link in soup.find_all("a"):
-                links.append(link)
-	return links
-def scrape_gazelle():
+for tag in soup.findAll(''):
+        print tag
+        imgUrl = tag.get('src')
+        imgName = imgUrl[imgUrl.rfind('/')+1:]
+        #urllib.URLopener().retrieve(imgUrl, imgName)
+        try:
+                urllib.URLopener().retrieve(imgUrl, imgName)
+        except (IOError):
+                pass
+'''
+
+
+
+###################################################################
+Gazelle
+###################################################################
+def scrape_gazelle(iphoneModel, carrier, capacity):
 	url = "http://buy.gazelle.com/buy/used/catalog/iphones"
 	links = url_scraper(url)
 	print links
 
+###################################################################
+Swappa
+###################################################################
 def scrape_swappa():
 	url = "https://swappa.com/buy/iphones"
 	links = url_scraper(url)
 	print links
 
+###################################################################
+USell
+###################################################################
+
 def main():
 	#print "Which site will you scrape?"
-	#scrape_gazelle()
-	scrape_swappa()
+	scrape_gazelle()
+	#scrape_swappa()
 main()
