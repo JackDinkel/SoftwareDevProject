a95ba4a6f2545f7655b304f42095b0816ac84d8b
Michael Swisher <Michael.V.Swisher@colorado.edu>
2015-11-14 23:56:55 -0700

    Updating scraper to have information about the database table setup

--- Scripts/scraper.py
+++ Scripts/scraper.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import urllib
+
 ###################################################################
-Reference Scraper
+#Reference Scraper
 ###################################################################
 '''
 url = ""
@@ -21,18 +22,30 @@ for tag in soup.findAll(''):
                 pass
 '''
 
-
-
 ###################################################################
-Gazelle
+#Gazelle
 ###################################################################
+#Example url:
+#https://www.gazelle.com/iphone/iphone-6s-plus/at-t/iphone-6s-plus-16gb-at-t/496033-gpid
 def scrape_gazelle(iphoneModel, carrier, capacity):
+	'''
 	url = "http://buy.gazelle.com/buy/used/catalog/iphones"
 	links = url_scraper(url)
 	print links
-
+	'''
+	url = "https://www.gazelle.com/iphone/iphone-6s-plus/at-t/iphone-6s-plus-16gb-at-t/496033-gpid"
+	r = requests.get(url)
+	#print r
+	data = r.text
+	#print data
+	soup = BeautifulSoup(data, "html.parser")
+	#print soup
+	#print iphoneModel, carrier, capacity
+	#for tag in soup.findAll('span',{'data':'oldoffer'}):
+	for tag in soup.findAll('h3'):
+		print tag
 ###################################################################
-Swappa
+#Swappa
 ###################################################################
 def scrape_swappa():
 	url = "https://swappa.com/buy/iphones"
@@ -40,11 +53,26 @@ def scrape_swappa():
 	print links
 
 ###################################################################
-USell
+#USell
 ###################################################################
+def scrape_usell():
+        url = "https://swappa.com/buy/iphones"
+        links = url_scraper(url)
+        print links
 
+###################################################################
+#Database Insertion
+###################################################################
+#DB Table format
+#brand|model|capacity|carrier|condition|price|site|timestamp
+def insert_to_db(brand, model, capacity, carrier, condition, price, site, timestamp):
+	x = 0 #Place holder
+	
+###################################################################
+#Main
+###################################################################
 def main():
 	#print "Which site will you scrape?"
-	scrape_gazelle()
+	scrape_gazelle("6s-plus", "at-t", "16gb")
 	#scrape_swappa()
 main()
