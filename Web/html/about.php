f3f0419ebbac4be9218dfd79b735af070418e6a3
dsgorthy <derek.gorthy@colorado.edu>
2015-12-01 20:30:52 -0700

    Updated text for contact and about pages

--- Web/html/about.php
+++ Web/html/about.php
@@ -1,7 +1,15 @@
 <?php include 'included/header.php'; ?>
 
+	<title>
+		About BBR
+	</title>
 	<section>
+		BuyBack Revolution is committed to transparency and customer satisfaction with our Highest Price Guarantee (link this).
+		We pay the most cash for newer used and broken smartphones, tablets and laptops. If you are selling your pre-owned device,
+		make sure you are getting the most for it! We are so confident that we will pay more, we compare our prices side-by-side with our competitors'.
 
+		We will earn your trust with an instant quote, free shipping label and quick payment! Stop getting less than you deserve for your devices,
+		let's do business together today!
 
 	</section>
 
--- Web/html/contact.php
+++ Web/html/contact.php
@@ -1,7 +1,10 @@
 <?php include 'included/header.php'; ?>
 
 	<section>
+		Have any questions? Send us an email for a quick response!
 
+		Email: questions@buybackrevolution.com
+		Phone: Coming soon!
 
 	</section>
 
