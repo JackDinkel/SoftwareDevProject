4324ef3c4c1d3b51ec2c95f35693664463f7150b
Michael Swisher <Michael.V.Swisher@colorado.edu>
2015-12-06 23:20:48 -0700

    Created legal page. btw 69 commit. check that shit yo LOL. SWISH OUT #DropsMic

---
+++ Web/html/legal.php
@@ -0,0 +1,32 @@
+<?php include 'included/header.php'; ?>
+	<section>
+		<h1>BUYBACK REVOLUTION LEGAL INFORMATION</h1>
+		<hr>
+		<p>
+		THIS DOCUMENT CONTAINS INFORMATION REGARDING THE FUNCTIONALITY AND LEGALITY OF OUR WEBSITE AND WEB SCRAPING TOOL. ANY REFERENCE TO "BBR", "WE" OR "US" REFERS TO BUYBACK REVOLUTION.<br><br>
+
+		1. About our Web Scraper<br><br>
+
+		We use a web scraper to obtain competing buyback prices from the following sites: Gazelle.com. An automated program searches there website and retrieves the prices and stores it in our database to display alongside our price.<br><br>
+
+		**NOTE: We do not use any competitor’s prices to calculate our own price. Our calculations are performed separately and scraped prices do not have any effect on our prices.<br><br>
+
+		We schedule our scraper to operate in a way that will not harass our competitions’ sites. We take calculated measures to ensure our usage of other websites do not exceed normal use.<br><br>
+
+		2. Concerning Other Websites’ Policies<br><br>
+
+		In this section, we will list out other website’s policies and address them explicitly.<br><br>
+
+		i. Gazelle.com<br><br>
+
+		Concerning Gazelle’s policy (7) “Automated Activity”. We do not agree to Gazelle’s terms and conditions – we have not created an account and therefore have no legal obligation to uphold their conditions. This information is publicly available and is not used to gain a competitive advantage.<br><br>
+
+		Referring to Facebook’s 2009 court case, BBR neither violates any copyrights nor causes any monetary damages. <br><br>
+
+		3. Accuracy of Competing Prices<br><br>
+
+		Our database is not updated constantly; therefore, all of our prices will be labeled by a timestamp of when the price information was received. We encourage all of our customers to independently check for the most accurate price on other websites. We are merely trying to provide customers with a convenient reference to typical prices on other sites. 
+
+		</p>
+	</section>
+<?php include 'included/footer.php'; ?>
