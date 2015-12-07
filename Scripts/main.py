6fb78f5225acd2bdc00d38298499d6e4d15e7de1
Jack <johnpauldinkel@gmail.com>
2015-12-06 22:29:02 -0700

    fixing bug

--- Scripts/main.py
+++ Scripts/main.py
@@ -409,7 +409,7 @@ def phpscript(phone):
 	#create a curser so we can query the db
 	cur = db.cursor()
 
-	print cur.execute("SELECT price FROM selling WHERE model='iphone-6s-plus' AND capacity='iphone-6s-plus-16gb-at-t' AND carrier=at-t")
+	print cur.execute("SELECT price FROM selling WHERE model='iphone-6s-plus' AND capacity='iphone-6s-plus-16gb-at-t' AND carrier='at-t'")
 
 
 if __name__ == '__main__':
