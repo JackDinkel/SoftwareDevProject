5a7672a8f0fe26bfde0cb15fb038a3a71eaa4877
Jack <johnpauldinkel@gmail.com>
2015-12-07 21:59:08 -0700

    lets try that again!

--- Web/html/calculate.php
+++ Web/html/calculate.php
@@ -81,7 +81,6 @@
 			</div> / 
 		</form>
 	<script type="text/javascript" src="js/formcalculations.js"></script>
-	<!--<?php echo "print technique 1:<br> ", $_GET['phone'] . ' ' .  $_GET['carrier'], "<br><br>"; ?> -->
 	<?php echo foreach($_GET as $key => $value) { echo $key . ': ' . $value . "<br>"; } ?>
 	<?php echo "Your phone is worth $360.00"; ?>
 <?php include 'included/footer.php'; ?>
